8d709dff3c9a50b444992dce1bdf1c23443d2148
RyoBamboo <itakedaka@gmail.com>
2016-03-08 13:55:22 +0900

    material-uiの導入

--- app.jsx
+++ app.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var RaisedButton = require('material-ui/lib/raised-button');
 
 var Hello = React.createClass( {
     render: function() {
         return(
             <div>
+                <RaisedButton label="Primary" secondary={true} />
             </div>
         )
     }
